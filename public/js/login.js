ab0baafffbe13065c0c2930b825e0d2de464a017
“Michael <michaelprattcoding@gmail.comgit config --global core.editor code>
2024-12-13 12:30:39 -0800

    Adding login error message

--- public/js/login.js
+++ public/js/login.js
@@ -17,7 +17,7 @@ const loginFormHandler = async (event) => {
       // If successful, redirect the browser to the profile page
       document.location.replace("/profile");
     } else {
-      alert(response.statusText);
+      alert("Unable to log in. Make sure you create an account.");
     }
   }
 };
